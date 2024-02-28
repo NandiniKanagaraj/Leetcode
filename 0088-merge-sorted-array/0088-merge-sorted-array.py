52b03319ec185d7ac358e0abb6c4a37680ab8d25
Nandini <158058465+NandiniKanagaraj@users.noreply.github.com>
2024-02-28 23:18:27 +0530

    Time: 18 ms (68.77%), Space: 11.8 MB (38.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         
-        for j in range(n):
-          nums1[m+j] = nums2[j]
+        for i in range(n):
+          nums1[m+i] = nums2[i]
         nums1.sort()
         print(nums1)
     
